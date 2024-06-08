7591625c345f6e9f3d04dfbb18a8e4b134fc31a3
ayoub ouahid <aouahid123@gmail.com>
2024-06-08 23:55:40 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { Home } from "./pages/Home/Home"
+
+const router = createBrowserRouter([
+  
+    {
+      path: '/',
+      element: <Home />,
+    },
+    
+  
+])
+
+function App() {
+
+  return (
+    <div className="app">
+        <RouterProvider router={router} />
+      </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,18 @@
+import './Home.scss';
+
+export const Home = () => {
+  return (
+    <div className="Home">
+        <div className="banniere">
+            <div className="item-1">
+                <img src="src/assets/img/logo-fleur.jpg" alt="img fleur" />
+                <h2>Fleurs Avec Passions</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis eius placeat consectetur aliquid amet, reiciendis quidem cupiditate iure ullam praesentium?</p>
+            </div>
+            <div className="item-2">
+                <img src="src/assets/img/banniere.jpg" alt="image grosse" />
+            </div>
+        </div>
+    </div>
+  );
+};
