c01c3ceafb49192db9d9555909bb0712f9496762
ayoub ouahid <aouahid123@gmail.com>
2024-06-12 21:29:21 +0200

    add section part to home Page

---
+++ src/components/Selection/Selection.tsx
@@ -0,0 +1,34 @@
+import "./Selection.scss"
+
+export const Selection = () => {
+  return (
+    <div className="selection">
+      <div className="white-box">
+        <div className="item-1">
+        <div className="img"></div>
+          <h2>Local</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio perferendis,
+            exercitationem natus ratione vitae veritatis?
+          </p>
+        </div>
+        <div className="item-2">
+        <div className="img"></div>
+          <h2>Local</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio perferendis,
+            exercitationem natus ratione vitae veritatis?
+          </p>
+        </div>
+        <div className="item-3">
+        <div className="img"></div>
+          <h2>Local</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio perferendis,
+            exercitationem natus ratione vitae veritatis?
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import './Home.scss';
 import { Banniere } from '../../components/Banniere/Banniere';
 import { Categorie } from '../../components/Categorie/Categorie';
+import { Selection } from '../../components/Selection/Selection';
 
 export const Home = () => {
   return (
     <div className="Home">
       <Banniere />
       <Categorie />
+      <Selection />
     </div>
   );
 };
