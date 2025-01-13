06335e12f3f4140354f7216320a370bf85fc7983
ayoub ouahid <aouahid123@gmail.com>
2025-01-13 16:40:20 +0100

    bannierre

---
+++ src/app/components/Bannierre/Bannierre.tsx
@@ -0,0 +1,15 @@
+import "../../style/Bannierre/Bannierre.scss";
+
+const Bannierre: React.FC = () => {
+  return (
+    <div className="bannierre">
+      <div className="text-container">
+        <h2>Flower Paris</h2>
+        <p>Espace Fleurs une entreprise familiale depuis plus de 40 ans. Avec deux boutiques dans le 19e arrondissement de Paris, notre solide expérience nous permet d'allier savoir-faire et qualité, pour ainsi proposer des fleurs et plantes les plus fraîches du marché au meilleur prix.</p>
+        <button>Découvrez nos Créations</button>
+      </div>
+    </div>
+  );
+};
+
+export default Bannierre;
