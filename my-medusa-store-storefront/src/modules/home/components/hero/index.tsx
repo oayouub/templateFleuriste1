4586927b110c24e768825cd128f328c88e4805ea
ayoub ouahid <aouahid123@gmail.com>
2025-01-26 13:39:41 +0100

    fix style front

--- my-medusa-store-storefront/src/modules/home/components/hero/index.tsx
+++ my-medusa-store-storefront/src/modules/home/components/hero/index.tsx
@@ -1,19 +1,20 @@
 import { Button, Heading } from "@medusajs/ui"
+import "../hero/hero.scss"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative hero">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-3xl leading-10 text-ui-fg-base font-normal text-teal-50"
           >
             Pretty Flower
           </Heading>
           <Heading
             level="h3"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+            className="text-3xl leading-10 text-ui-fg-subtle font-normal text-pink-200"
           >
             Beauté et Savoir Faire
           </Heading>
